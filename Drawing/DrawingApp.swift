0bbc589f6a497bef179efdcb7ec24dc9349930dd
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-05-21 09:13:57 +0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Arkasha Zuev on 21.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
